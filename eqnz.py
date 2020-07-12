1da91bd585906be466063f94d303eea8e8cbc7c9
Petra Lamborn <petra@petras.space>
2020-07-12 22:46:39 +1200

    Possibly working, but there's been no earthquakes in 2 hours to test it so who knows!

--- eqnz.py
+++ eqnz.py
@@ -1,11 +1,10 @@
 import time
-import signal
 import requests
 import dateutil.parser
-import datetime
 import argparse
 from sys import stderr
 from pytz import timezone
+from mastodon import Mastodon
 
 roman = {1: "I (Unnoticeable)",
          2: "II (Unnoticeable)",
@@ -20,31 +19,41 @@ roman = {1: "I (Unnoticeable)",
          11: "XI (Extreme)",
          12: "XII (Extreme)"}
 
+
 def parseArgs():
     parser = argparse.ArgumentParser(description="EQNZ mastodon bot")
     parser.add_argument("-m", "--mmi", type=int, default=-1, dest="mmi",
-                        help="minimum Modified Mercalli Intensity (MMI).",
-                        choices=range(-1,8), metavar = "[-1 to 8]")
-    parser.add_argument("-s", "--secret", type=str, metavar = "[file]",
+                        help=("minimum Modified Mercalli Intensity (MMI), "
+                              "default: -1"),
+                        choices=range(-1, 8), metavar="[-1 to 8]")
+    parser.add_argument("-t", "--time", type=int, default=30, dest="wait",
+                        metavar="[INT]",
+                        help="seconds between API polls, default: 30")
+    parser.add_argument("-s", "--secret", type=str, metavar="[file]",
                         default="eqnz_usercred.secret",
-                        help="mastodon secret file", dest="secret")
+                        help=("mastodon secret file, default: "
+                              "eqnz_usercred.secret"), dest="secret")
     parser.add_argument("-d", "--debug", dest="debug", action="store_true",
                         help="do not post to mastodon")
     return parser.parse_args()
 
+
 def latestQuakes(mmi):
     try:
         resp = requests.get("https://api.geonet.org.nz/quake",
-            headers = {"Accept": "application/vnd.geo+json;version=2"},
-            params = {"MMI": mmi})
+                            headers={"Accept":
+                                     "application/vnd.geo+json;version=2",
+                                     "User-Agent": "Mastodon eqnz bot"},
+                            params={"MMI": mmi})
 
         eqjs = resp.json()["features"]
         eqs = {}
         for eq in eqjs:
             thiseq = {
                 "coordinates": eq["geometry"]["coordinates"],
-                "time": dateutil.parser.isoparse(eq["properties"]["time"]).
-                            astimezone(tz=timezone("Pacific/Auckland")),
+                "time": dateutil.parser.isoparse(eq[
+                    "properties"][
+                        "time"]).astimezone(tz=timezone("Pacific/Auckland")),
                 "depth": round(float(eq["properties"]["depth"]), 1),
                 "magnitude": round(float(eq["properties"]["magnitude"]), 1),
                 "mmi": eq["properties"]["mmi"],
@@ -57,14 +66,15 @@ def latestQuakes(mmi):
         print(e, file=stderr)
         return {}
 
-def printEQ(eq, eqid, threadid, args):
+
+def printEQ(eq, eqid, threadid, args, masto):
     global roman
     if threadid is not None:
         ps1 = "Update on earthquake "
     else:
         ps1 = "Earthquake "
     ps2 = ("at {time:%-I:%M %p}, {locality}.\nMagnitude {magnitude:.1f}, at "
-          "a depth of {depth:.1f} kilometres.").format(**eq)
+           "a depth of {depth:.1f} kilometres.").format(**eq)
     if eq["mmi"] in roman:
         ps3 = "\nModified Mercalli Intensity: {}".format(roman[eq["mmi"]])
     else:
@@ -74,29 +84,36 @@ def printEQ(eq, eqid, threadid, args):
     else:
         ps4 = ""
     if threadid is None:
-        ps5 = "\nhttps://www.geonet.org.nz/earthquake/{}".format(eqid)
+        ps5 = "\nhttps://www.geonet.org.nz/earthquake/{} #eqnz".format(eqid)
     else:
         ps5 = ""
     post = "{}{}{}{}{}".format(ps1, ps2, ps3, ps4, ps5)
     print(post)
-    if args.debug:
-        print("noMasto")
+    if not args.debug:
+        try:
+            return masto.status_post(post, in_reply_to_id=threadid)["id"]
+        except Exception as e:
+            print(e, file=stderr)
+            return None
     else:
-        print("Masto")
+        # Placeholder id
+        return "123456789"
 
-    # Placeholder id
-    return "123456789"
 
 def main():
     args = parseArgs()
+    if not args.debug:
+        mastodon = Mastodon(access_token=args.secret)
+    else:
+        mastodon = None
+
     eqs = latestQuakes(args.mmi)
 
     if len(eqs) == 0:
         raise ValueError("Failed to get first batch of earthquakes")
 
     while True:
-        print("looping...")
-        time.sleep(60)
+        time.sleep(args.wait)
         neweq = latestQuakes(args.mmi)
         for qid in neweq:
             if neweq[qid]["quality"] == "deleted":
@@ -108,13 +125,25 @@ def main():
                         eqs[qid]["locality"] != neweq[qid]["locality"]):
                     # Modified entry
                     tid = (eqs[qid]["threadid"] if
-                        "threadid" in eqs[qid] else None)
-                    neweq[qid]["threadid"] = printEQ(neweq[qid], qid, tid, args)
+                           "threadid" in eqs[qid] else None)
+                    neweq[qid]["threadid"] = printEQ(neweq[qid], qid, tid,
+                                                     args, mastodon)
                     eqs[qid] = neweq[qid]
             else:
                 # New entry
-                neweq[qid]["threadid"] = printEQ(neweq[qid], qid, None, args)
+                neweq[qid]["threadid"] = printEQ(neweq[qid], qid, None, args,
+                                                 mastodon)
                 eqs[qid] = neweq[qid]
+        # If dict starts getting long, remove last (by date)
+        # API documentation claims it returns maximum of 100 entries
+        if len(eqs) > 500:
+            lasteq = sorted(list(eqs.keys()), key=lambda x:
+                            eqs[x]["time"].isoformat(),
+                            reverse=False)[0]
+            print(("Removing earthquake {}"
+                   " ({:%c})").format(lasteq, eqs[lasteq]["time"]))
+            eqs.pop(lasteq)
+
 
 if __name__ == "__main__":
     main()
