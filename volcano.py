5814761f30d92c6509e7c5db6b3844eeefc9d6ff
Petra Lamborn <petra@petras.space>
2020-07-14 10:12:08 +1200

    Volcano script

--- volcano.py
+++ volcano.py
@@ -1,6 +1,23 @@
 import requests
 import json
 from sys import stderr
+import argparse
+from mastodon import Mastodon
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description=("EQNZ mastodon bot: volcano "
+                                                  "alert levels"))
+    parser.add_argument("-j", "--json", type=str, metavar="FILE",
+                        default="volc.json",
+                        help=("json file, default: "
+                              "volc.json"), dest="jfile")
+    parser.add_argument("-s", "--secret", type=str, metavar="FILE",
+                        default="eqnz_usercred.secret",
+                        help=("mastodon secret file, default: "
+                              "eqnz_usercred.secret"), dest="secret")
+    parser.add_argument("-d", "--debug", dest="debug", action="store_true",
+                        help="do not post to mastodon, do print initial")
+    return parser.parse_args()
 
 
 def getNewVolcData():
@@ -21,7 +38,7 @@ def saveVolcData(dat, fn):
         file.write(dat)
 
 
-def updateVolc(volfeat, oldval):
+def updateVolc(volfeat, oldval, args, mastodon):
     newval = volfeat["properties"]["level"]
     chword = "downgraded" if oldval >= newval else "upgraded"
     volid = volfeat["properties"]["volcanoID"]
@@ -33,17 +50,33 @@ def updateVolc(volfeat, oldval):
             "#VolcanoNZ").format(volc, chword, oldval,
                                  newval, altext, volid)
     print(sval)
+    if not args.debug:
+        try:
+            masto.status_post(sval)
+        except Exception as e:
+            print(e, file=stderr)
+
 
 
 def main():
+    args = parseArgs()
+    if not args.debug:
+        mastodon = Mastodon(access_token=args.secret)
+    else:
+        mastodon = None
     nd = getNewVolcData()
     ndj = json.loads(nd)
+    if args.debug:
+        for vol in ndj["features"]:
+            print("{}: {} ({})".format(vol["properties"]["volcanoTitle"],
+                                       vol["properties"]["level"],
+                                       vol["properties"]["activity"]))
     try:
-        od = loadOldVolcData("volc.json")
+        od = loadOldVolcData(args.jfile)
         odj = json.loads(od)
     except Exception as e:
         print(e, file=stderr)
-        saveVolcData(nd, "volc.json")
+        saveVolcData(nd, args.jfile)
         quit()
     for vi in range(0, len(odj["features"])):
         try:
@@ -51,11 +84,12 @@ def main():
             ndv = ndj["features"][vi]
             if (odv["properties"]["volcanoID"] ==
                     ndv["properties"]["volcanoID"] and
-                    odv["properties"]["level"] != ndv["properties"]["level"]):
+                    int(odv["properties"]["level"]) !=
+                    int(ndv["properties"]["level"])):
                 updateVolc(ndv, odv["properties"]["level"])
         except Exception as e:
             print(e, file=stderr)
-    saveVolcData(nd, "volc.json")
+    saveVolcData(nd, args.jfile)
 
 
 if __name__ == "__main__":
